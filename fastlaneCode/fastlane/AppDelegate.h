6b3b156267f2feb00ce8a79b07c8ae7f5bf0db91
chengchao <1244053432@qq.com>
2017-02-20 10:02:06 +0800

    创建工程

---
+++ fastlaneCode/fastlane/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  fastlane
+//
+//  Created by chao on 17/2/20.
+//  Copyright © 2017年 chao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ fastlaneCode/fastlane/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  fastlane
+//
+//  Created by chao on 17/2/20.
+//  Copyright © 2017年 chao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
